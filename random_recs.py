835c05c6d2b69d675437d1b98af5bee9a0cadb05
Alessandra <aapolimeno@gmail.com>
2022-04-12 12:53:17 +0200

    added random recommendations

---
+++ random_recs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 11 13:36:39 2022
+
+@author: alessandrapolimeno
+"""
+
+import pandas as pd 
+import random
+
+
+# ============================= Load data =============================
+full_data = pd.read_csv("../../data/eval_data_hlgd_t.csv")
+full_data = full_data.drop(["Unnamed: 0", "chain_id"],
+                           axis=1) # drop irrelevant columns
+
+index = full_data.index
+full_data["article_id"] = index # add column with article ids based on index
+full_data.to_csv("../../data/article_ids_train.csv")
+
+# ================== Generate random recommendations ================== 
+
+users = [i for i in range(1000)] # 100 users 
+
+random_recs = pd.DataFrame()
+random_recs["user_id"] = users
+
+recs = []
+
+for user in users: 
+    rec = random.choices(index, k = 15) # 15 recs per user 
+    recs.append(rec)
+
+random_recs["recs"] = recs
+random_recs.to_csv("../../data/random_recs.csv", index=False)
+
